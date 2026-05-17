feace26a1720e1ca2ce1b37528fc62892315b324
Abdullah Zeynel <0abdullahzeynel0@gmail.com>
2026-05-17 13:33:31 +0300

    fixes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react(),tailwindcss()],
-  base: command === 'build' ? '/alitepe_vakfi_web_sitesi/' : '/',
+  base: command === 'build' ? '/alitepe_vakfi_site/' : '/',
 }))
