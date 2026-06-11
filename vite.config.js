f84c1be16e4fb184290a94f8e75fe5ee571f5e66
Abdullah Zeynel <0abdullahzeynel0@gmail.com>
2026-06-11 13:42:42 +0300

    yenietkinlik

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react(), tailwindcss()],
-  base: command === 'serve' ? '/' : '/alitepe_vakfi_site/',
+  base: '/',
 }))
